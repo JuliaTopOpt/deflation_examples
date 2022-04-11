1632414388dfe87c5f8eaab01e7a6ad620d91a4a
Yijiang Huang <yijiangh@mit.edu>
2022-04-10 22:10:44 -0400

    conto left over results

--- scripts/variational_inference/experiments.jl
+++ scripts/variational_inference/experiments.jl
@@ -8,8 +8,7 @@ include(srcdir("var_inf.jl"))
 general_args = Dict(
     "seed"     => 123,
     "mean_amp" => 30,
-    "mean_dim" => Array(10:1:20),
-    "nsolves"  => 10,
+    "mean_dim" => Array(5:1:10),
     "nsamples" => 10,
     "niters"   => 10000,
     "power"    => 3.0,
@@ -22,7 +21,7 @@ arg_dicts = dict_list(general_args)
 
 pbar = Progress(length(arg_dicts), 1, "VI multi-instance experiments...")
 for (i, args) in enumerate(arg_dicts)
-    @unpack seed, mean_amp, mean_dim, nsolves, nsamples, niters, power, radius = args
+    @unpack seed, mean_amp, mean_dim, nsamples, niters, power, radius = args
     println(args)
 
     rng = StableRNG(seed)
@@ -30,7 +29,7 @@ for (i, args) in enumerate(arg_dicts)
     means = mean_amp * randn(rng, mean_dim)
 
     objectives, runtimes, plt = stochastic_vi(means, initθ, rng;
-        nsolves=nsolves, power=power, niters=niters, nsamples=nsamples, radius=radius, 
+        power=power, niters=niters, nsamples=nsamples, radius=radius, 
         verbose=verbose)
     result_data = merge(copy(args), @strdict objectives runtimes)
 
